70c6aa505490806b64ed43b0b48a61f7a1351b1d
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2023-05-04 01:20:51 -0300

    04/05/2023

---
+++ js/carrossel.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+  
+    },
+
+  });
