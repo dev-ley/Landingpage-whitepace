f2171f706e5a4f33bb32af4961ac8f7b46ac3a3d
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2023-05-09 12:26:15 -0300

    terça de manha

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,11 +1,30 @@
 const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 10,
+    // Default parameters
     slidesPerView: 1,
+    spaceBetween: 10,
+    effect: 'slide',
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
   
     },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      // when window width is >= 640px
+      1152: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
+  })
 
-  });
