b4794954e65c13f9609a2d377e0e08aff3ee51b6
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2023-05-11 09:09:05 -0300

    1

--- js/carrossel.js
+++ js/carrossel.js
@@ -28,3 +28,32 @@ const swiper = new Swiper('.swiper', {
     }
   })
 
+  const swiper2 = new Swiper('.swiper2', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    effect: 'slide',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+  
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      700: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      // when window width is >= 640px
+      1152: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    }
+  })
