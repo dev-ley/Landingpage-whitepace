c3df55201acd6b4b36b0abff27c131ab0ba07c9b
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2023-05-15 19:43:12 -0300

    1

--- js/cepauto.js
+++ js/cepauto.js
@@ -1,19 +1,30 @@
-async function buscaEndereco (cep) {
-    try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+async function buscaEndereco(cep) {
+
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+     try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
-            throw Error ('CEP não existente!');
+            throw Error('CEP não existente!');
         }
-        console.log (consultaCEPConvertida); 
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
-    } catch (erro) {
-        console.log(erro)
+        } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro);
     }
 }
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
-
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
