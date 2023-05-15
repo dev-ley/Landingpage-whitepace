42855bf3cd0b1b0ba52a41fa1747f667d2d45855
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2023-05-15 18:53:29 -0300

    loguin inicio

---
+++ js/cepauto.js
@@ -0,0 +1,19 @@
+async function buscaEndereco (cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error ('CEP não existente!');
+        }
+        console.log (consultaCEPConvertida); 
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
