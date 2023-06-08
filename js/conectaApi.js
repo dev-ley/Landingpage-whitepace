f65ae8de246fcb1835700c1fedae7ab7bc5bf350
Wesley Rodrigues <ley.desenvolvedor@gmail.com>
2023-06-08 20:23:31 -0300

    att

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,5 +1,5 @@
 async function listaCards() {
-    const conexao = await fetch ("https://landingpage-whitepace.vercel.app/db.json");
+    const conexao = await fetch ("http://localhost:3000/cards");
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
